abc76bd2230525297e446fd777726fd11cf3d71c
Amiya Sekhar <amiyasekhar@Amiyas-MBP-2.hsd1.ma.comcast.net>
2024-07-07 13:07:28 -0400

    amiya changes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,23 +8,9 @@ const LandingPage = () => {
   <div style={{ backgroundColor: '#ED1504', minHeight: '100vh', fontFamily: 'Bebas Neue', }}>
     <div style={{ display: 'flex', justifyContent: 'space-between', padding: '16px', alignItems: 'center', marginRight: 'auto', marginLeft: '20%', display: 'flex', gap: '16px', fontFamily: 'Bebas Neue',  }}>
       <Typography variant="h4" style={{ color: '#FFFFFF', flex: '0 1 auto' }}>
-        Swipe Mate
+        SwipeMate
       </Typography>
-        <div style={{ marginLeft: 'auto', marginRight: '20%', display: 'flex', gap: '16px' }}>
-          <NavBar/>
-          {/* <Link href="#" color="inherit" variant="button" underline="none" style={{ color: '#BBBBBB', }}>
-            Features
-          </Link>
-          <Link href="#" color="inherit" variant="button" underline="none" style={{ color: '#BBBBBB', }}>
-            Login
-          </Link>
-          <Link href="#" color="inherit" variant="button" underline="none" style={{ color: '#BBBBBB', }}>
-            Download
-          </Link>
-          <Link href="#" color="inherit" variant="button" underline="none" style={{ color: '#BBBBBB',}}>
-            Contact Us
-          </Link> */}
-        </div>
+
       </div>
 
       <Container style={{ paddingTop: '16px', paddingBottom: '16px', fontFamily: 'Bebas Neue', }}>
@@ -36,7 +22,7 @@ const LandingPage = () => {
           <Grid container direction="column" alignItems="center" justifyContent="center" height="100%">
             <Grid item>
             <Typography variant="h2" component="h2" sx={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: '4.5rem', fontFamily: 'Bebas Neue',}}>
-              It helps you get the hottest bitches! Go from Simping to Pimping
+              Let AI swipe your Tinder! Go from Simping to Pimping in 3 easy steps
             </Typography>
             </Grid>
           </Grid>
